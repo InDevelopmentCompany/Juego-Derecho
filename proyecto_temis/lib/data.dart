5ce46926c120f630f6890dadd017c4cbb6701b40
Jorge Hurtado <jahurtadod@outlook.com>
2019-07-10 17:08:59 -0500

    feat(): update app

---
+++ proyecto_temis/lib/data.dart
@@ -0,0 +1,13 @@
+List<String> imagenes = [
+  "assets/daft.jpg",
+  "assets/halo.jpg",
+  "assets/kda.jpg",
+  "assets/abogado.jpg",
+];
+
+List<String> title = [
+  "Caso 4- Parte 4",
+  "Caso 3- Parte 3",
+  "Caso 2- Parte 2",
+  "Caso 1- Parte 1",
+];
